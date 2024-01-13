88d282eafa930cae73f77f58fe2a858d4494521e
Magomed <magometsapraliev@gmail.com>
2024-01-13 16:39:20 +0300

    8 homework without styles

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,51 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  switch (action.type) {
+    case "sort": {
+      const CopyState = [...state];
+      if (action.payload === "up") {
+        CopyState.sort((a, b) => {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
 
-            return state // need to fix
-        }
-        case 'check': {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+      } else {
+        CopyState.sort((a, b) => {
+          const nameA = a.name.toUpperCase();
+          const nameB = b.name.toUpperCase();
 
-            return state // need to fix
-        }
-        default:
-            return state
+          if (nameA < nameB) {
+            return 1;
+          }
+          if (nameA > nameB) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+
+      return CopyState;
     }
-}
+    case "check": {
+      const CopyState = state.filter((u) => u.age >= 18);
+      return CopyState;
+    }
+    default:
+      return state;
+  }
+};
